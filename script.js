723ac2c7237bc9d791e78a5dc660304c8a607557
andersoncasantos <andersonmtv07@gmail.com>
2023-11-26 11:10:08 -0300

    Create Modal

--- script.js
+++ script.js
@@ -5,9 +5,19 @@ const form = document.querySelector('form');
 const inputWage = document.querySelector('#wage');
 const inputPercentage = document.querySelector('#percentage');
 
-const modalWrapper = document.querySelector('.modal-wrapper');
-const modalMessage = document.querySelector('.modal .title span');
-const modalBtnClose = document.querySelector('.modal button.close');
+const Modal = {
+
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .title span'),
+    buttonClose: document.querySelector('.modal button.close'),
+
+    open(){
+        Modal.wrapper.classList.add('open');
+    },
+    close() {
+        Modal.wrapper.classList.remove('open');
+    }
+}
 
 form.onsubmit = event => {
     event.preventDefault();
@@ -17,12 +27,12 @@ form.onsubmit = event => {
 
     const result = CalculoSoftware(wage, percentage);
     const message = `O valor do software é R$ ${result.toFixed(2)}`;
-    modalMessage.innerText = message;
-    modalWrapper.classList.add('open');
+    Modal.message.innerText = message;
+    Modal.open();
 }
 
-modalBtnClose.onclick = () => {
-    modalWrapper.classList.remove('open');
+Modal.buttonClose.onclick = () => {
+    Modal.close();
 }
 
 function CalculoSoftware(wage, percentage){
